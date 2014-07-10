3c56fcbf1ca647be5b6d281ec991013e13987088
Sailesh Mittal <smittal@twitter.com>
2014-07-10 15:17:23 -0700

    Add description of images.

--- Tweme/MemeBook.swift
+++ Tweme/MemeBook.swift
@@ -15,15 +15,15 @@ class MemeBook {
     
     init() {
         book = []
-        book.append(Meme(description: "Hello1", image: UIImage(named: "1.jpg")))
-        book.append(Meme(description: "Hello1", image: UIImage(named: "2.jpg")))
-        book.append(Meme(description: "Hello1", image: UIImage(named: "3.jpg")))
-        book.append(Meme(description: "Hello1", image: UIImage(named: "4.jpg")))
-        book.append(Meme(description: "Hello1", image: UIImage(named: "5.jpg")))
-        book.append(Meme(description: "Hello1", image: UIImage(named: "6.jpg")))
-        book.append(Meme(description: "Hello1", image: UIImage(named: "7.jpg")))
-        book.append(Meme(description: "Hello1", image: UIImage(named: "8.jpg")))
-        book.append(Meme(description: "Hello1", image: UIImage(named: "9.jpg")))
+        book.append(Meme(description: "Success Kid", image: UIImage(named: "1.jpg")))
+        book.append(Meme(description: "Socially Awkward Penguin", image: UIImage(named: "2.jpg")))
+        book.append(Meme(description: "Futurama Fry", image: UIImage(named: "3.jpg")))
+        book.append(Meme(description: "Most Interesting Man", image: UIImage(named: "4.jpg")))
+        book.append(Meme(description: "Philosoraptor", image: UIImage(named: "5.jpg")))
+        book.append(Meme(description: "Condescending Wonka", image: UIImage(named: "6.jpg")))
+        book.append(Meme(description: "Boromir", image: UIImage(named: "7.jpg")))
+        book.append(Meme(description: "Drunk Baby", image: UIImage(named: "8.jpg")))
+        book.append(Meme(description: "Baby Godfather", image: UIImage(named: "9.jpg")))
     }
     
     func size() -> Int {
